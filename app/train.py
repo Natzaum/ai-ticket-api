db6e4d594e1530b4038ccdd71896f9ef3113e95a
Natzaum <natacezerbordignon@gmail.com>
2025-12-31 21:08:54 -0300

    Feat: adding train model to score and classify tickets.

--- app/train.py
+++ app/train.py
@@ -3,3 +3,28 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from data.loader import DatasetLoader
+
+def train_models():
+  loader = DatasetLoader()
+  df = loader.load()
+  
+  category_pipeline = Pipeline([
+    ('vectorizer', TfidfVectorizer()),
+    ('classifier', MultinomialNB())
+  ])
+  
+  priority_pipeline = Pipeline([
+    ('vectorizer', TfidfVectorizer()),
+    ('classifier', MultinomialNB())
+  ])
+  
+  category_pipeline.fit(df['description'], df['category'])
+  priority_pipeline.fit(df['description'], df['category'])
+  
+  joblib.dump(category_pipeline, 'model_category.joblib')
+  joblib.dump(priority_pipeline, 'model_priority.joblib')
+  
+  print("Training complete.")
+  
+if __name__ == "__main__":
+  train_models()
