0d71d588b917d07008e3a1c31c9528d6f95e63c4
Natzaum <natacezerbordignon@gmail.com>
2026-01-12 18:55:58 -0300

    feat: Using especific error handler for validations.

--- app/handlers.py
+++ app/handlers.py
@@ -3,7 +3,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 
-async def validation_exception_handler(request: Request, exc: Exception):
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
     return JSONResponse(
         status_code=422,
         content={
