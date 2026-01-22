11320b09afeff6c6bd5acdf45f9cb02abf430f2b
Natzaum <natacezerbordignon@gmail.com>
2026-01-22 17:03:28 -0300

    feat: Adding in-memory database. Creating a new endpoint.

--- app/main.py
+++ app/main.py
@@ -8,18 +8,28 @@ app = FastAPI(title="AI Ticket Classifier API")
 
 app.add_exception_handler(RequestValidationError, validation_exception_handler)  # type: ignore
 
+tickets_db: list[TicketResponse] = []
+
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"API": "Alive"}
 
 
-@app.post("/tickets/classify", response_model=TicketResponse)
-def classify(ticket: TicketRequest):
+@app.post("/tickets/classify/", response_model=TicketResponse)
+async def classify(ticket: TicketRequest):
     result = ml_service.predict(ticket.description)
 
-    return TicketResponse(
+    ticket_response = TicketResponse(
         category=result["category"],
         priority=result["priority"],
         confidence=result["confidence"],
     )
+
+    tickets_db.append(ticket_response)
+    return ticket_response
+
+
+@app.get("/tickets/")
+async def get_all_tickets():
+    return tickets_db
